9b48e5a7d766db305cee31f5a74a7d547365fd01
Dylan Duke <laytonmaes@gmail.com>
2022-08-04 13:23:33 -0600

    Add buttons to main page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ function App() {
       <header className="App-header">
         <h1>Inspiration Ink.</h1>
       </header>
+        <main>
+          <div className='random-buttons'>
+          <button> Inspirational Word </button>
+          <button> Series Mashups </button>
+          </div>
+
+          <div className='user-buttons'>
+          <button> Random User Prompts </button>
+          <button> Submit Your Own Prompt! </button>
+          </div>
+        </main>
     </div>
   );
 }
